99419208fcf8533f13caae7e9d52aaeb5aa46df3
prioleaud <wilhemnia@yahoo.com>
2018-07-08 20:31:54 -0700

    Python scripts for K-fold and RemoveOnly functions

---
+++ RemoveOnly.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  8 20:01:45 2018
+@filename:  RemoveOnly.py
+@date:      8 Jul 2018
+@author:    Joshua Peeples and Diandra Prioleau
+"""
+import csv
+import numpy as np
+import pdb 
+import os
+from sklearn.neighbors import NearestNeighbors
+
+def RemoveOnly(data,k):
+    
+    k_prime = (k + 1)/2;
+    
+    S = data; 
+    ncolumns = len(S[1]) - 1;
+    
+    num_removes = 0; #counts number of samples removed 
+    temp = S[:,0:ncolumns-1]; #dataset without column labelling glass type
+    
+    i = 0; #variable to control while loop
+    
+    while i < len(temp): #loop based on size of temp
+    
+        print('i: ', i)
+        temp = S[:,0:ncolumns-1];
+        temp = np.delete(temp,i,0);
+        
+        #finding k nearest neighbors for each sample in training data
+        nbrs = NearestNeighbors(n_neighbors=k, algorithm='brute').fit(temp) 
+        distances, indices = nbrs.kneighbors(temp);
+        
+        count_nlabels = 0;
+        temp_indices = indices[i,:];
+        
+        for j in range(0,len(temp_indices)):
+            index = indices[i,j]; #
+            if(S[index,ncolumns] == S[i,ncolumns]):
+                count_nlabels += 1;
+        
+        print(count_nlabels)
+        if(count_nlabels < k_prime):
+            print('remove');
+            print('i before: ', i)
+            #remove sample from training data
+            S= np.delete(S,i,0);
+            num_removes += 1;
+        else :
+            i += 1;
+            
+            print('i after: ', i)
+    
+    #return data remaining after executing RemoveOnly algorithm 
+    return S;
+    
---
+++ kfold.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+@filename:  10_kfold.py
+@date:      8 Jul 2018 
+@author:    Joshua Peeples & Diandra Prioleau
+"""
+import numpy as np
+from sklearn.model_selection import KFold
+import csv
+import pdb
+import os
+from RemoveOnly import RemoveOnly
+
+#Reads in glass identification from file 
+filename = 'C:\\Users\\Diandra\\Documents\\HXR Lab\\AQ Project\\Experiment\Original Datasets\\glass_id_original.csv';
+raw_data = open(filename, 'rt');
+reader = csv.reader(raw_data, delimiter=',',quoting=csv.QUOTE_NONE);
+x = list(reader);
+data = np.array(x).astype('float');
+print(data.shape);
+
+k = 10; #10-fold cross validation 
+kf = KFold(n_splits=k, random_state=None, shuffle=True);
+
+#Conduct RemoveOnly
+remove_only = True;
+
+#output filenames for training and test data
+output_training = 'kfold_output_training';
+output_test = 'kfold_output_test';
+
+trial_num = 1;
+file_num = 0;
+
+#Create directory to save data for each new trial
+path = os.getcwd();
+
+#append trial number 
+path = path + '\\Trial' + str(trial_num);
+
+#make directory 
+if not os.path.exists(path):
+    os.makedirs(path);
+
+for train_index, test_index in kf.split(data):
+    print("TRAIN:", train_index, "TEST:", test_index);
+    data_train, data_test = data[train_index], data[test_index]
+
+    #Write training and test data to file 
+    np.savetxt(path + '\\' + output_training + str(file_num) + '.csv', data_train, delimiter=",");
+    np.savetxt(path + '\\' + output_test + str(file_num) + '.csv', data_test, delimiter=",");
+    
+   
+    if(remove_only):
+        #Call RemoveOnly function for each training set
+        remove_only_data = RemoveOnly(data_train,k)
+        
+        #Save data remaining from RemoveOnly 
+        np.savetxt(path + '\\' + 'remove_only' + str(file_num) + '.csv', remove_only_data, delimiter=",");
+    
+    file_num += 1;
