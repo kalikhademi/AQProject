9e8047976ef6d59c0ae4f0e7f9077e1601727a00
Princess Lyons <plyons126@outlook.com>
2018-07-27 14:59:21 -0400

    added relative paths

--- main.py
+++ main.py
@@ -22,45 +22,44 @@ def ImportData(path):
     x = list(reader);
     data = np.array(x).astype('float');
     print(data.shape);
-    
+
     return data;
 
 #kNN classification
 def Classifier(k,train_data,test_data):
-    
+
     #pdb.set_trace();
     ncolumns = len(train_data[1]) - 1;
-    
-    #kNN Classifier 
+
+    #kNN Classifier
     knn = KNeighborsClassifier(n_neighbors=k);
-            
+
     #fitting the model
     knn.fit(train_data[:,1:ncolumns], train_data[:,ncolumns]);
-            
+
     #predict the response
     pred = knn.predict(test_data[:,1:ncolumns]) ;
-    
+
     return pred;
-    
-    
+
+
 #variables
 percent_50 = 0.5;
 percent_80 = 0.8;
 numTrials = 4;
 k_neighbors = 3;
 
-#Import AQ dataset 
-AQ_50 = ImportData('C:\\Users\\Diandra\\Documents\\HXR Lab\\AQ Project\\Experiment\\Code\\Original Datasets\\recommended-applications_50.csv');
-
-AQ_80 = ImportData('C:\\Users\\Diandra\\Documents\\HXR Lab\\AQ Project\\Experiment\\Code\\Original Datasets\\recommended-applications_80.csv');
+#Import AQ dataset
+AQ_50 = ImportData(os.path.join(os.path.dirname(__file__), 'data_files/recommended-applications_50.csv'))
+AQ_80 = ImportData(os.path.join(os.path.dirname(__file__), 'data_files/recommended-applications_80.csv'))
 
 
 #TODO: Random 70-30 (training/test) split on dataset - ask Armisha for function that calls random generator
-#will be removed later 
-train_data = ImportData('C:\\Users\\Diandra\\Documents\\HXR Lab\\AQ Project\\Experiment\\Code\\Original Datasets\\glass_id_train.csv');
-test_data = ImportData('C:\\Users\\Diandra\\Documents\\HXR Lab\\AQ Project\\Experiment\\Code\\Original Datasets\\glass_id_test.csv');
+#will be removed later
+train_data = ImportData(os.path.join(os.path.dirname(__file__), 'data_files/glass_id_train.csv'))
+test_data = ImportData(os.path.join(os.path.dirname(__file__), 'data_files/glass_id_test.csv'))
 
-#Call SubSelect - returns percentage of training set 
+#Call SubSelect - returns percentage of training set
 train_50 = SubSelect(percent_50,numTrials,train_data);
 
 train_80 = SubSelect(percent_80,numTrials,train_data);
@@ -70,7 +69,7 @@ train_80 = SubSelect(percent_80,numTrials,train_data);
 for i in range(0,numTrials):
     print('Trial_50',i)
     AQeval(test_data[:,-1],Classifier(k_neighbors,np.array(train_50[i]),test_data));
-        
+
     print('Trial_80',i)
     AQeval(test_data[:,-1],Classifier(k_neighbors,np.array(train_80[i]),test_data));
 
